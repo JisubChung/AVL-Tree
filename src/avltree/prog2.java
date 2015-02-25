4998b54545984abc35fcd9207c8b0aeea1a2cc55
Ji-sub Chung <jisub.chung0@gmail.com>
2015-02-24 18:31:10 -0800

    Added insert method, cleaned up
    Filled in code for insert, cleaned up various comments and
    differentiated mine v. professor's comments

--- src/avltree/prog2.java
+++ src/avltree/prog2.java
@@ -7,17 +7,17 @@
 package avltree;
 
 class StringAVLNode {
-	
+
 	private String val;
 	private int balance;
 	private StringAVLNode left, right;
-	
-	//PROF: I believe we have all agreed that one constructor should suffice
+
+	// PROF: I believe we have all agreed that one constructor should suffice
 	public StringAVLNode(String str) {
 		this.balance = 0;
 		this.val = str;
 	}
-	
+
 	public int getBalance() {
 		return balance;
 	}
@@ -37,7 +37,7 @@ class StringAVLNode {
 	public void setLeft(StringAVLNode pt) {
 		this.left = pt;
 	}
-	
+
 	public StringAVLNode getRight() {
 		return right;
 	}
@@ -45,112 +45,138 @@ class StringAVLNode {
 	public void setRight(StringAVLNode pt) {
 		this.right = pt;
 	}
-} //PROF: StringAVLNode
-	
+} // PROF: StringAVLNode
+
 class StringAVLTree {
 
 	private StringAVLNode root;
-	
-	//PROF: the one and only constructor
+
+	// PROF: the one and only constructor
 	public StringAVLTree() {
-	 
+
 	}
-	
-	//PROF:  this is here to make it easier for me to write a test
-	//PROF:  program – you would never do this in real life!
+
+	// PROF: this is here to make it easier for me to write a test
+	// PROF: program – you would never do this in real life!
 	public StringAVLNode getRoot() {
-		
+
 	}
-	
-	//PROF:  Rotate the node to the right
-	public StringAVLNode rotateRight(StringAVLNode t) {
-		
+
+	// PROF: Rotate the node to the right
+	private static StringAVLNode rotateRight(StringAVLNode t) {
+
 	}
-	
-	//PROF:  Rotate the node to the left
-	public StringAVLNode rotateLeft(StringAVLNode t) {
-		
+
+	// PROF: Rotate the node to the left
+	private static StringAVLNode rotateLeft(StringAVLNode t) {
+
 	}
-//PROF:  For these next four, be sure not to use any global variables
-	
-	//PROF:  Return the height of the tree – not to be used anywhere in insert or delete
+
+	// PROF: For these next four, be sure not to use any global variables
+
+	// PROF: Return the height of the tree – not to be used anywhere in insert
+	// or delete
 	public int height() {
-		
+
 	}
-	
-	//PROF:  Return the number of leaves in the tree
+
+	// PROF: Return the number of leaves in the tree
 	public int leafCt() {
-		
+
 	}
-	
+
 	// Return the number of nodes with exactly one non-null child
 	public int stickCt() {
-		
+
 	}
-	
-	//PROF: Return the inorder successor or null if there is none or num is not in the tree
+
+	// PROF: Return the inorder successor or null if there is none or num is not
+	// PROF: in the tree
 	public StringAVLNode successor(String str) {
-		
+
 	}
-	
+
+	// Driver Method for insert
 	public void insert(String str) {
-		
+		root = insert(str, root);
 	}
-	
-	private StringAVLNode insert(String str, StringAVLNode t) {
-		if (t == null) { // easiest case – inserted node goes here
-		
+
+	private static StringAVLNode insert(String str, StringAVLNode t) {
+		if (t == null) { // PROF: easiest case – inserted node goes here
+			t = new StringAVLNode(str);
 		}
-		else if (t.getVal() == str) { // already in the tree – do nothing
-			
-		}
-		
-		else if ... { // str is smaller than this node – go left
-			
-			// get the old balance of the left child (where the insertion
-			// is taking place)
-			if (t.getLeft() == null) {
-				oldBalance =
+		else if (t.getVal().equals(str)) {} // PROF: already in the tree – do nothing
+		else if (str.compareToIgnoreCase(t.getVal()) < 0) { // PROF: str is smaller than this node – go left
+			int oldBalance, newBalance;
+			// PROF: get the old balance of the left child (where the insertion
+			// PROF: is taking place)
+			if (t.getLeft() == null) { //THIS IS A SPECIAL CASE
+				oldBalance = 0; //??????????????????????? why can we not set this to 0?
+								//??????????????????????? afaik this won't cause problems?
+				//There are 2 cases where t's left node is null
+				//Case 1: t already has a right child
+				if (t.getBalance() == 1) {
+					//so after the insertion the balance of t will be 0
+					t.setBalance(0);
+				}
+				//Case 2: t is a leaf
+				else {
+					//so after the insertion the balance of t will be -1
+					t.setBalance(-1);
+				}
 			}
 			else {
 				oldBalance = t.getLeft().getBalance();
 			}
-		}
-		
-		t.setLeft(insert(str, t.getLeft()));
-		newBalance = t.getLeft().getBalance();
-		if ... { // did the height increase?
-			// fix the balance value
-		
-			if (t.getBalance == -2) {// out of balance – must rotate
-				if (t.getLeft().getBalance() == -1){ // single rotation
-												// and balance update
-					else { 				// double rotation
-									// and balance update
-						// once you get it right here, basically the
-						// same code can be used in other places,
-						// including delete
+			t.setLeft(insert(str, t.getLeft()));
+			newBalance = t.getLeft().getBalance();
+			if (oldBalance == 0 && newBalance != 0) { // PROF: did the height increase?
+				// PROF: fix the balance value
+				t.setBalance(t.getBalance()-1);
+				if (t.getBalance() == -2) {// PROF: out of balance – must rotate
+					if (t.getLeft().getBalance() == -1){ // PROF: single rotation
+						rotateRight(t);
+						// PROF: and balance update
+						t.setBalance(0);
+						t.getLeft().setBalance(0);
 					}
+					else { 				// PROF: double rotation
+						// PROF: and balance update
+						boolean leftRightLeftInsertion;
+						if (t.getLeft().getRight().getBalance() == -1) {
+							leftRightLeftInsertion = true;
+						}
+						if (leftRightLeftInsertion) {
+							t.setBalance(1);
+							t.getLeft().setBalance(0);
+						}
+						else {
+							t.setBalance(0);
+							t.getLeft().setBalance(-1);	
+						}
+						t.getLeft().getRight().setBalance(0);
+						rotateLeft(t);
+						rotateRight(t);
+						
+						// PROF: once you get it right here, basically the
+						// PROF: same code can be used in other places,
+						// PROF: including delete
+					}					
 				}
 			}
 		}
-		else { // str is bigger than this node
+		
+		else { // PROF: str is bigger than this node
 		
 		}
 	
 	return t;
 	}
-	
+
 	public void delete(String str) {
-		
+
 	}
 
-	
-	
-	
-	
-	
-	
 	private StringAVLNode delete(StringAVLNode t, String str) {
 
 		if (t == null) {
@@ -223,33 +249,35 @@ class StringAVLTree {
 			//PROF: check to see if only need to change balance values or rotate
 		}
 	}
-		
-		
-	//PROF: The code to find and replace the node being deleted must be recursive
-	//PROF: so that we have easy access to the nodes that might have balance changes
-	private StringAVLNode replace(StringAVLNode t, StringAVLNode prev, StringAVLNode replacement) {
-		if (replacement.getRight() == null) { 	//PROF: at the node that will replace
-												//PROF: the deleted node
-			if (prev != null) { 	//PROF: if replacement node is NOT the child
-									//PROF: ... a special case
+
+	// PROF: The code to find and replace the node being deleted must be
+	// recursive
+	// PROF: so that we have easy access to the nodes that might have balance
+	// changes
+	private StringAVLNode replace(StringAVLNode t, StringAVLNode prev,
+			StringAVLNode replacement) {
+		if (replacement.getRight() == null) { // PROF: at the node that will
+												// replace
+												// PROF: the deleted node
+			if (prev != null) { // PROF: if replacement node is NOT the child
+								// PROF: ... a special case
 			}
-			//PROF: move the replacement node – Recall there is no setVal
-		}
-		else {
+			// PROF: move the replacement node – Recall there is no setVal
+		} else {
 
-			//PROF: find the old balance	
-			
+			// PROF: find the old balance
+
 			t = replace(t, replacement, replacement.getLeft());
-			
-			//PROF: find the new balance
-			
-			//PROF: update balance and rotate if needed
+
+			// PROF: find the new balance
+
+			// PROF: update balance and rotate if needed
 		}
 	}
-	
-	//PROF: who are you? Put your name here!
+
+	// PROF: who are you? Put your name here!
 	public static String myName() {
-	return "Ji-Sub Chung";
+		return "Ji-Sub Chung";
 	}
 
-} //PROF: end of StringAVLTree class
+} // PROF: end of StringAVLTree class
